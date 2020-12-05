bf657912505c30399f964432fdab85034d7fc8fb
penglingss <manypeoplek@gmail.com>
2020-12-05 23:02:15 +0900

    upload files

---
+++ src/test/java/com/in/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
